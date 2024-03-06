38ef13198244121263aef40d5f168a13d54029d0
Ilhamsyah Putra <ilhamsyah.putraa1997@gmail.com>
2024-03-06 22:22:19 +0700

    Push

--- components/home/Companies.tsx
+++ components/home/Companies.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Companies = () => {
   return (
-    <div className='h-[100vh] flex items-center justify-center'>
-      Kompany
+    <div className=' grid items-center justify-center lg:px-[120px] px-4'>
+      {/*  */}
+      <div className='lg:flex grid gap-5'>
+      <div className="w-1/2 text-white lg:text-5xl font-medium font-['Space Grotesk'] lg:leading-[60px] text-2xl   leading-loose">Trusted by 200+ companies around the world</div>
+      <div className="mt-6 lg:mt-7 text-white text-opacity-60 text-base font-normal font-['Pathway Extreme'] leading-normal">Vulputate molestie molestie amet leo blandit accumsan. Sapien sed amet tellus purus sit odio eget. Diam morbi faucibus vitae neque id in. Nullam sed et dapibus nunc, porta enim orci urna, sit. Lectus ac.</div>
+      </div>
+
+      {/* Logos */}
+      <div>
+        baris pertama
+      </div>
+      <div>
+        Baris kedua
+      </div>
     </div>
   )
 }
